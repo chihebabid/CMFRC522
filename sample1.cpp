f15485f5930293c8d2b2f012d127f8976d74c2e3
abid <chiheb.abid@gmail.com>
2021-05-04 18:55:47 +0100

    Cleanup code

--- sample1.cpp
+++ sample1.cpp
@@ -1,9 +1,11 @@
 //============================================================================
 // Name        : pigpioLED.cpp
 // Author      : CA ABID
-// Version     :
+// Version     : 0.2
 // Copyright   : 2021
-// Description : Test sample using LCD driver
+// Description : Test sample using CMFRC522 class : detect the presence of a new card
+//               and print its serial number
+//               To compile this program, use g++ sample1.cpp -lpigpio -lpthread
 //============================================================================
 
 #include "CMFRC522.h"
@@ -14,26 +16,13 @@
 #include <sstream>
 
 using namespace std;
-//#define PIGPIO 1
+
 int main(int argc, char *argv[]) {
-	/*if (gpioInitialise() < 0) {
-		cout << "Failure..." << endl;
-		exit(-1);
-	}*/
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
 	CMFRC522 mfrc522;
-
-
-   // mfrc522.PCD_Init();   // Initiate MFRC522
-    //mfrc522.PCD_Reset();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout<<"Initialization success"<<endl;
+    // Initializes PCD
     mfrc522.PCD_Init();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    //mfrc522.PCD_PerformSelfTest();// std::cout<<"Self test passed..."<<std::endl;
-    //exit(0);
     while(1) {
-        if (mfrc522.isNewCardPresent()==0) {
+        if (mfrc522.isNewCardPresent()==0) { // Detect whether a new card is present
             std::cout << "New card is here..." << std::endl;
             mfrc522.RC522_Anticoll(mfrc522._id);
             for (uint8_t i = 0; i < 8; i++) {
@@ -41,7 +30,5 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-	gpioTerminate();
-	return 0;
 }
 
